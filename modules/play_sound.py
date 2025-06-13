9767ad233a99bfad816af7e179327e4296bc29ae
zeingal <zeingal@icloud.com>
2025-06-13 16:29:33 +0200

    Update play_sound.py

--- modules/play_sound.py
+++ modules/play_sound.py
@@ -1,8 +1,21 @@
+# modules/play_sound.py
+
+import os
 import pygame
+from modules.assets_manager import get_asset_path
 
-def init_sound_system():
-    pygame.mixer.init()
+# Initialiser Pygame Mixer une seule fois
+pygame.mixer.init()
 
-def play_sound(file_path):
-    pygame.mixer.music.load(file_path)
-    pygame.mixer.music.play()
+# Fonction pour jouer le son de notification (boom.wav)
+def play_notification():
+    try:
+        sound_path = get_asset_path("sounds", "boom.wav")
+        if not os.path.exists(sound_path):
+            print("[ERREUR] Le fichier boom.wav est introuvable.")
+            return
+        sound = pygame.mixer.Sound(sound_path)
+        sound.play()
+        print("[INFO] Son de notification joué.")
+    except Exception as e:
+        print(f"[ERREUR] lors de la lecture de boom.wav : {e}")
