5a4998ef8f7ef88d392c04bf025b6e7072e11722
zeingal <zeingal@icloud.com>
2025-06-13 10:01:23 +0200

    Update interface.py

--- modules/interface.py
+++ modules/interface.py
@@ -3,8 +3,8 @@ from modules import (
     start_animation,
     theme_manager,
     display,
-    pages_home,
     toast,
+    navigator,
 )
 from modules.logger import setup_logger
 
@@ -21,21 +21,21 @@ def run_app():
         root.configure(bg="black")
         root.attributes("-fullscreen", True)
 
-        # Application du thème (jour/nuit)
+        # Application du thème
         theme_manager.apply_theme(root)
 
         # Réglage initial de la luminosité
         display.set_brightness_from_config()
 
-        # Fonction de démarrage après animation
+        # Fonction de démarrage après l’animation
         def launch_main_page():
             logger.info("[SPYNBOOX] Chargement de la page d'accueil")
-            pages_home.display_home_page(root)
+            navigator.navigate_to("home", root)
 
         # Animation de démarrage
         start_animation.play_start_animation(root, launch_main_page)
 
-        # Lancement de l'application
+        # Boucle principale
         root.mainloop()
 
     except Exception as e:
