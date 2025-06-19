f538c190a46d35d9202a1c248ca20ce43a48fa1a
zeingal <zeingal@icloud.com>
2025-06-19 14:21:52 +0200

    Create main_test.py

---
+++ main_test.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+def run_app():
+    root = tk.Tk()
+    root.title("SPYNBOOX TEST")
+    root.geometry("480x320")
+    root.configure(bg="black")
+    root.mainloop()
+
+if __name__ == "__main__":
+    run_app()
